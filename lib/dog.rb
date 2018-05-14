94bb36e969bf6321e65e245736a7758aeb575e76
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-14 17:52:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog #this is the class
   def name=(dog_name) #this instance method is 'setting'
     @this_dogs_name = dog_name
+    #use the @ symbol to set an instance varible inside a class
   end
 
   def name #this instance method that reads the dog's name is 'getting'
