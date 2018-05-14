55c1604f207f6a7f14e929ef3091aec008592ea5
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-14 17:51:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 class Dog #this is the class
   def name=(dog_name) #this instance method is 'setting'
-    this_dogs_name = dog_name
-    #this_dogs_name is a local variable with a local scope
+    @this_dogs_name = dog_name
   end
 
   def name #this instance method that reads the dog's name is 'getting'
-    this_dogs_name
+    @this_dogs_name
 
     lassie = Dog.new
     lassie.name = "Lassie"
