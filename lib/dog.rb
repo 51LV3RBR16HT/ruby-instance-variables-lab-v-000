d2553dd6d065b90d0788251ef20e5418d480cfa0
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-14 17:47:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
-class Dog
-  def name=(dog_name)
-    this_dogs_name = dog_name
+class Dog #this is the class
+  def name=(dog_name) #this instance method is 'setting'
+    this_dogs_name = dog_name #
   end
 
-  def name
+  def name #this instance method that reads the dog's name is 'getting'
     this_dogs_name
   end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name
